b90e85c983cc87f063c0d811d9f573dbb67a7253
EliasAla <90680749+EliasAla@users.noreply.github.com>
2023-06-26 20:04:33 +0300

    osa2 teht. 2.16

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,18 +3,20 @@ import axios from "axios";
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
+import personService from "./services/persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [newFilter, setNewFilter] = useState("");
+  const [addMessage, setAddMessage] = useState(null);
 
   useEffect(() => {
     console.log("effect");
-    axios.get("http://localhost:3001/persons").then((response) => {
+    personService.getAll().then((initialPersons) => {
       console.log("promise fulfilled");
-      setPersons(response.data);
+      setPersons(initialPersons);
     });
   }, []);
 
@@ -26,17 +28,26 @@ const App = () => {
       window.alert(`${newName} is already added to phonebook`);
     } else if (newName.length < 1) {
       window.alert("Field cannot be empty");
+    } else if (newNumber.length < 1) {
+      window.alert("Field cannot be empty");
     } else {
       const nameObject = {
         name: newName,
         number: newNumber,
       };
 
-      setPersons(persons.concat(nameObject));
+      setAddMessage(`Added '${newName}' to phonebook`);
+      setTimeout(() => {
+        setAddMessage(null);
+      }, 5000);
 
-      // Tyhjentää input kentät
-      setNewName("");
-      setNewNumber("");
+      personService.create(nameObject).then((returnedObject) => {
+        // Asettaa nykyiseksi taulukon johon lisätty uusi objekti
+        setPersons(persons.concat(returnedObject));
+        // Tyhjentää input kentät
+        setNewName("");
+        setNewNumber("");
+      });
     }
   };
 
@@ -57,9 +68,38 @@ const App = () => {
     return person.name.toLowerCase().includes(newFilter.toLowerCase());
   });
 
+  const handleDeletePerson = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      personService.deletePerson(id).then(() => {
+        setPersons(persons.filter((n) => n.id !== id));
+
+        setAddMessage(`Removed '${name}' from phonebook`);
+        setTimeout(() => {
+          setAddMessage(null);
+        }, 5000);
+
+        setNewName("");
+        setNewNumber("");
+      });
+    }
+  };
+
+  const Notification = ({ message }) => {
+    if (message === null) {
+      return;
+    } else if (message.includes("Removed")) {
+      return <div className="removeMessage">{message}</div>;
+    } else {
+      return <div className="addMessage">{message}</div>;
+    }
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <Notification message={addMessage} />
+
       <Filter value={newFilter} handle={handleFilterChange} />
 
       <h2>add a new</h2>
@@ -72,7 +112,7 @@ const App = () => {
       />
 
       <h2>numbers</h2>
-      <Persons persons={personsToShow} />
+      <Persons persons={personsToShow} deletePerson={handleDeletePerson} />
     </div>
   );
 };
--- osa2/puhelinluettelo/src/components/Persons.js
+++ osa2/puhelinluettelo/src/components/Persons.js
@@ -1,9 +1,12 @@
-const Persons = ({ persons }) => {
+const Persons = ({ persons, deletePerson }) => {
   return (
     <ul>
       {persons.map((person) => (
         <li key={person.name}>
           {person.name} {person.number}
+          <button onClick={() => deletePerson(person.id, person.name)}>
+            Delete
+          </button>
         </li>
       ))}
     </ul>
